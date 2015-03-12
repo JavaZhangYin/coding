3f06fabd8459e0aca9bf46e44b89e38da26a7175
Shumin Guo <gsmsteve@gmail.com>
2015-03-12 07:49:49 -0700

    Re-implementation of option parsing using a more uniersal method.

--- alexa/candy/options.c
+++ alexa/candy/options.c
@@ -1,35 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "options.h"
 
 void parseOptions(int argc, char** argv, DELAYS *dlys) {
-  opterr = 0; 
-  char *val; // the value of an option. 
+  if(argc < 2) return;
+  int i = 1; 
+  while(i < argc) {
+    if(argv[i][0] == '-') {
+      int opt = argv[i][1]; // the option letter.
+      if (i >= argc - 1) return; 
 
-  int o; 
-  while((o = getopt(argc, argv, "E:L:f:e:")) != -1) {
-    switch(o) {
-    case 'E':
-      dlys->delay_frog = atoi(optarg); 
-      printf("option %s\n", optarg); 
-      break; 
-    case 'L':
-      dlys->delay_escargot = atoi(optarg); 
-      break; 
-    case 'f':
-      dlys->delay_lucy = atoi(optarg); 
-      break; 
-    case 'e':
-      dlys->delay_ethel = atoi(optarg); 
-      break; 
-    case '?':
-      if (optopt == 'c')
-	fprintf(stderr, "Option -%c requires an argument. ", optopt);
-
-      else if (isprint (optopt))
-	fprintf(stderr, "Unknown option -%c.\n", optopt);
-      else perror("Unknown option character."); 
-      return; 
-    default: 
-      return; 
+      char *optval = argv[i+1]; 
+      switch (opt) {
+      case 'E':
+	dlys->delay_frog = atoi(optval); 
+	printf("option %s\n", optval); 
+	break; 
+      case 'L':
+	dlys->delay_escargot = atoi(optval); 
+	break; 
+      case 'f':
+	dlys->delay_lucy = atoi(optval); 
+	break; 
+      case 'e':
+	dlys->delay_ethel = atoi(optval); 
+	break; 
+      default: 
+	fprintf(stderr, "Unknow option -%c.\n", opt); 
+      }
     }
+    printf("arg[%d] = %s\n", i, argv[i]);
+    i++;
   }
 }
+
+// for testing. 
+/* int main(int argc, char** argv) { */
+
+/*   DELAYS * dl = (DELAYS *)malloc(sizeof(DELAYS)); */
+/*   if(!dl) return -1;  */
+/*   parseOptions(argc, argv, dl); */
+
+/*   printf("Frog Delay: %d\n", dl->delay_frog );  */
+/*   printf("Escargot Delay: %d\n", dl->delay_escargot );  */
+/*   printf("Ethel Delay: %d\n", dl->delay_ethel );  */
+/*   printf("Lucy Delay: %d\n", dl->delay_lucy );  */
+/*   return 0; */
+/* } */
