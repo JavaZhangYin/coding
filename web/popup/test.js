633194acd7fcefa43bbf14d499dca7271c92d766
Shumin Guo <gsmsteve@gmail.com>
2015-03-18 22:15:43 -0700

    The magnific popup example.

---
+++ web/popup/test.js
@@ -0,0 +1,33 @@
+$(function () {
+    $('.popup-image').magnificPopup({
+	type: 'image'
+    });
+
+    $('.popup-gallery').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	tLoading: 'Loading image #%curr%...',
+	mainClass: 'mfp-img-mobile',
+	gallery: {
+	    enabled: true,
+	    navigateByImgClick: true,
+	    preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	},
+	image: {
+	    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+	    titleSrc: function(item) {
+		return item.el.attr('title') + '<small>by Shumin Guo</small>';
+	    }
+	}
+    });
+
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	disableOn: 700,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 160,
+	preloader: false,
+	
+	fixedContentPos: false
+    });
+});
