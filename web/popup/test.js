aea8f1b4e49ed864eb1f5c666ab190e4ba01e936
Shumin Guo <gsmsteve@gmail.com>
2015-03-19 23:17:53 -0700

    Working popup modal using magnific popup.

--- web/popup/test.js
+++ web/popup/test.js
@@ -30,4 +30,14 @@ $(function () {
 	
 	fixedContentPos: false
     });
+    $('.popup-modal').magnificPopup({
+	type: 'inline',
+	preloader: false,
+	focus: '#username',
+	modal: true
+    });
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+	e.preventDefault();
+	$.magnificPopup.close();
+    });
 });
