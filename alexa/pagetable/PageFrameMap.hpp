f50f6d47f715cbc811c12175003201e54f2d01b4
Shumin Guo <shumin.guo@experian.com>
2015-04-26 18:45:55 -0700

    Added page map.

---
+++ alexa/pagetable/PageFrameMap.hpp
@@ -0,0 +1,32 @@
+#ifndef _PAGE_FRAME_MAP_H_
+#define _PAGE_FRAME_MAP_H_
+
+using namespace std; 
+
+// map virtual page to a frame page. 
+// this class will maintain a static 
+// variable so that we know what's the 
+// currently mapped frame. 
+
+class PageFrameMap {
+  
+ public: 
+  PageFrameMap(); 
+  int getFrame() const; 
+  int setFrame(int f); 
+  int next(); // get the next frame for mapping. 
+
+  /**
+   * Return the physical address with given offset. 
+   * @param offset the address offset relative to the page.
+   * @return the physical address. 
+   */
+  int getPhysicalAddress(int offset); 
+
+ private: 
+  static int current; // currently mapped page. 
+  int frame;	      // frame address.
+  
+}; 
+
+#endif
